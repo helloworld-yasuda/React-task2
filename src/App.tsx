e2b51f856be9f89e0cc5a3a8ece3b8e0b0b02a64
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-07-06 17:54:20 +0900

    React課題2_安田

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import EmployeeCard from './EmployeeCard'
+import employees from'./Employees'
+
+const App = () => {
+  return (
+      <div className="characterContainer">
+        <h1>Employees</h1>
+        {employees.map((employee) => (
+          <EmployeeCard
+            key = {employee.id}
+            name = {employee.name}
+            role = {employee.role}
+            email = {employee.email}
+            />
+        ))
+        }
+      </div>
+  )
+}
+
+export default App;
